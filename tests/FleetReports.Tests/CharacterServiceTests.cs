0b3a01746e710e4dffa5387982878fcc2b748e7c
WellseyInSpaceAgain <WellseyInSpaceAgain>
2026-05-05 19:21:40 +0100

    add character service tests

---
+++ tests/FleetReports.Tests/CharacterServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using FleetReports.Services;
+using Moq;
+
+namespace FleetReports.Tests;
+
+public class CharacterServiceTests
+{
+    [Fact]
+    public async Task ResolveNamesAsync_ReturnsNameToIdMapping()
+    {
+        var mock = new Mock<IEsiService>();
+        mock.Setup(x => x.PostAsync<UniverseIdsResponse>("universe/ids/", It.IsAny<object>()))
+        .ReturnsAsync(new UniverseIdsResponse([
+            new CharacterEntry(12345, "Wellsey"),
+            new CharacterEntry(67890, "Anotherchar"),
+        ]));
+
+        var service = new CharacterService(mock.Object);
+        var result = await service.ResolveNamesAsync(["Wellsey", "Anotherchar"]);
+
+        Assert.Equal(12345, result["Wellsey"]);
+        Assert.Equal(67890, result["Anotherchar"]);
+    }
+
+    [Fact]
+    public async Task ResolveNamesAsync_ExcludesUnknownNames()
+    {
+        var mock = new Mock<IEsiService>();
+        mock.Setup(x => x.PostAsync<UniverseIdsResponse>("universe/ids/", It.IsAny<object>()))
+            .ReturnsAsync(new UniverseIdsResponse([
+                new CharacterEntry(12345, "Wellsey")
+            ]));
+
+        var service = new CharacterService(mock.Object);
+        var result = await service.ResolveNamesAsync(["Wellsey", "NotARealPlayer"]);
+
+        Assert.Single(result);
+        Assert.False(result.ContainsKey("NotARealPlayer"));
+    }
+}
